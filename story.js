c81ddc1eab3de97dec17b913fa89a81ed73dac8a
vanderson19 <andersonvy13@gmail.com>
2017-07-20 10:47:43 -0700

    Update story.js

--- story.js
+++ story.js
@@ -1,2 +1,6 @@
 function submit(){
-document.getElementById("input").innerHTML="Welcome " + document.getElementById("Name").value
+document.getElementById("story").innerHTML="In the future… /n
+As of yet there are been no successful attempts to change our course towards Global Warming,
+as a result temperatures have been rising, there is acid rain, rising sea levels, mass extinction of vital crops and livestock, as well as unexpected and devastating natural disasters with no ozone layer, the polar ice caps have disappeared"
++ document.getElementById("Name").value + " it’s up to you to save the world!"
+}
