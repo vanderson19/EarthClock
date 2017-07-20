42a544e89a64707ad3a076fe7d946d3c1c1689a1
vanderson19 <andersonvy13@gmail.com>
2017-07-20 11:24:15 -0700

    Update story.js

--- story.js
+++ story.js
@@ -1,4 +1,4 @@
-// function submit(){
-// document.getElementById("story").innerHTML="In the future…As of yet there are been no successful attempts to change our course towards Global Warming, as a result temperatures have been rising, there is acid rain, rising sea levels, mass extinction of vital crops and livestock, as well as unexpected and devastating natural disasters with no ozone layer, the polar ice caps have disappeared "
-// + document.getElementById("Name").value + ", it's up to you to you to save the planet!"
-// }
+function submit(){
+document.getElementById("story").innerHTML="In the future…As of yet there are been no successful attempts to change our course towards Global Warming, as a result temperatures have been rising, there is acid rain, rising sea levels, mass extinction of vital crops and livestock, as well as unexpected and devastating natural disasters with no ozone layer, the polar ice caps have disappeared "
++ document.getElementById("Name").value + ", it's up to you to you to save the planet!"
+}
